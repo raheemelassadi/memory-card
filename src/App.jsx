04d7ffec424743137190ebae00188b4b96b35c7b
raheem <raheem@raheems-MBP.attlocal.net>
2025-01-04 20:31:28 -0800

    added on click animation to each card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import './App.css';
 
 function App() {
 
@@ -13,23 +14,26 @@ function App() {
       })
   }, [])
 
+  function tester(){
+    console.log("hello")
+  }
+
   if (!data){
     return <div>Loading...</div>
   }
 
   return (
-    <>
-    <h1 className="">ATLA - Memory Game</h1>
-    <div className="grid grid-cols-2 grid-rows-3 gap-4">
-        { data && data.map((item) => {
+    <div className="p-14">
+    <h1 className="text-center text-lg font-bold pb-8">ATLA - Memory Game</h1>
+    <div className="grid grid-cols-2 grid-rows-3 gap-6">
+        { data.map((item) => {
           return(
-            <div key={item._id} className="p-4 rounded bg-gray-200">
-              <img src={item.photoUrl} alt="" className="w-full h-auto rounded cursor-pointer"/>
+            <div key={item._id} className="p-4 rounded bg-gray-200 items" onClick={tester}>
+              <img src={item.photoUrl} alt="" className="w-full h-auto rounded cursor-pointer "/>
             </div>
         )})}
-
       </div>
-    </>
+    </div>
   )
 }
 
